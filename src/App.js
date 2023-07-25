cb3d8d991b1728072c61d0c42b8d117ebd253352
youasasd <youa.asd25@gmail.com>
2023-07-25 09:29:15 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from "react";
-//import Web3 from "web3";
+import Web3 from "web3";
 
 const contractAddress = "0xE221F82448D06ba5aB4CE0D5815Df06Ef33A5b15"; // Replace with your existing contract address
 const contractABI = [
